af48f43ecba9841947ab8a9e465eeaecfbc0d8c4
Kefeng He <kefuhe86@gmail.com>
2024-08-07 23:33:32 +0800

    deleted:    .gitattributes 	deleted:    csi_cutde_mpiparallel 	deleted:    eqtools 	modified:   install.bat 	modified:   install.sh

--- install.bat
+++ install.bat
@@ -1,8 +1,5 @@
 @echo off
 
-REM 初始化和更新子模块 --remote: 保持和子模块的更新同步，如果不更新则移除
-git submodule update --init --recursive --remote
-
 REM 安装eqtools
 cd eqtools
 pip install .
--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 初始化和更新子模块 --remote: 保持和子模块的更新同步，如果不更新则移除
-git submodule update --init --recursive --remote
-
 # 安装eqtools
 cd eqtools
 pip install .
