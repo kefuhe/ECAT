d1a7e39a914ead50d8061d75693f23ec8184bb81
Kefeng He <kefuhe86@gmail.com>
2024-08-21 17:01:16 +0800

    deleted:    examples/SMC_Geometry_Slip_Bayesian_Inversion/Taiwan_20240405Mw7_4/Nonlinear/InvFaultInfo_multidatas_smc_mag_rake.py 	new file:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Taiwan_20240405Mw7_4/Nonlinear/default_config.yml 	modified:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Taiwan_20240405Mw7_4/Nonlinear/descript.ion 	modified:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Taiwan_20240405Mw7_4/Nonlinear/kde_matrix.png 	new file:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Taiwan_20240405Mw7_4/Nonlinear/kde_matrix_F0.png 	modified:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Taiwan_20240405Mw7_4/Nonlinear/samples_final.h5 	new file:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Taiwan_20240405Mw7_4/Nonlinear/samples_mag_rake_multifaults.h5 	new file:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Taiwan_20240405Mw7_4/Nonlinear/test_nonlinear_mag_rake.py 	modified:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/default_config.yml 	new file:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/kde_matrix_F0.png 	modified:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/kde_matrix_sigmas.png 	new file:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/model_results_mean.json 	modified:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/samples_final.h5 	modified:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/samples_mag_rake_multifaults.h5 	modified:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/sar_S1_T012A_ifg_data_map.png 	modified:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/sar_S1_T012A_ifg_res_map.png 	modified:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/sar_S1_T012A_ifg_synth_map.png 	modified:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/sar_S1_T121D_ifg_data_map.png 	modified:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/sar_S1_T121D_ifg_res_map.png 	modified:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/sar_S1_T121D_ifg_synth_map.png 	renamed:    examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/InvFaultInfo_multidatas_smc_mag_rake_multifaults.py -> examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/test_nonlinear_mag_rake.py 	modified:   examples/SMC_Geometry_Slip_Bayesian_Inversion/Wushi_20240122M7_0/Nonlinear/default_config.yml 	renamed:    examples/SMC_Geometry_Slip_Bayesian_Inversion/Wushi_20240122M7_0/Nonlinear/InvFaultInfo_multidatas_smc_mag_rake_multifaults.py -> examples/SMC_Geometry_Slip_Bayesian_Inversion/Wushi_20240122M7_0/Nonlinear/test_nonlinear_mag_rake.py

---
+++ examples/SMC_Geometry_Slip_Bayesian_Inversion/Taiwan_20240405Mw7_4/Nonlinear/test_nonlinear_mag_rake.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+from eqtools.csiExtend.exploremultifaults_smc import explorefault
+from csi.gps import gps
+from csi.insar import insar
+from mpi4py import MPI
+import matplotlib.pyplot as plt
+
+import os
+# using the C++ backend
+# os.environ['CUTDE_USE_BACKEND'] = 'cpp'
+
+
+if __name__ == '__main__':
+    # -----------------------------------MPI Init---------------------------------------------#
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    # -----------------------------------Read Data---------------------------------------------#
+    # Read the data
+    lon0 = 122
+    lat0 = 24
+
+    # 
+    verbose = False
+
+    # Read GNSS Data
+    gpsfile = os.path.join('..', r'GPS', 'us7000m9g4_forweb_NGL.txt')
+    mygps = gps('gps', lon0=lon0, lat0=lat0, verbose=verbose)
+    mygps.read_from_enu(gpsfile, header=2, factor=1)
+    mygps.vel_enu[:, 2] = np.nan
+    mygps.buildCd(direction='en')
+    err_mean = mygps.err_enu[:, :2].mean()
+
+    geodata = [mygps]
+    #------------------------------Set ExploreFault Object-------------------------------------#
+    expfault = explorefault('invrc', lat0=lat0, lon0=lon0, config_file='default_config.yml', geodata=geodata, verbose=verbose)
+    nchains = expfault.nchains
+    chain_length = expfault.chain_length
+    expfault.setPriors(bounds=None, initialSample=None, datas=None) # datas for Sar reference
+    expfault.setLikelihood(datas=None, verticals=None) 
+    # expfault.walk(nchains=nchains, chain_length=chain_length, comm=comm, filename='samples_mag_rake_multifaults.h5', 
+    #               save_every=2, save_at_interval=False, covariance_epsilon = 1e-9, amh_a=1.0/9.0, amh_b=8.0/9.0)
+
+    # ---------------------------------Plot Results---------------------------------------------#
+    if rank == 0:
+        expfault.load_samples_from_h5(filename='samples_mag_rake_multifaults.h5')
+        expfault.print_mcmc_parameter_positions()
+        # Plot Faults
+        for ifault, faultname in enumerate(expfault.faultnames):
+            expfault.plot_kde_matrix(save=True, plot_faults=True, faults=faultname, fill=True, 
+                                    scatter=False, filename=f'kde_matrix_F{ifault}.png')
+        # expfault.plot_kde_matrix(save=True, plot_faults=False, plot_sigmas=True, fill=True, 
+        #                          scatter=False, filename='kde_matrix_sigmas.png')
+        faults = expfault.returnModels()
+
+        # Plot GPS
+        for data in [mygps]:
+            # Build the synthetics
+            data.buildsynth(faults, vertical=False)
+        mygps.plot(drawCoastlines=True, data=['data', 'synth'], scale=0.2, legendscale=0.05, color=['k', 'r'])
+
---
+++ examples/SMC_Geometry_Slip_Bayesian_Inversion/Western_Xizang_20200722M6.3/Nonlinear/test_nonlinear_mag_rake.py
@@ -0,0 +1,104 @@
+import numpy as np
+import pandas as pd
+# from myexplorefault_smc import MyExploreFault as explorefault
+from eqtools.csiExtend.exploremultifaults_smc import explorefault
+from csi.gps import gps
+from csi.insar import insar
+from mpi4py import MPI
+import matplotlib.pyplot as plt
+
+import os
+# using the C++ backend
+# os.environ['CUTDE_USE_BACKEND'] = 'cpp'
+
+
+if __name__ == '__main__':
+    # -----------------------------------MPI Init---------------------------------------------#
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    # -----------------------------------Read Data---------------------------------------------#
+    lon0 = 86.83
+    lat0 = 33.16
+
+    # ---------------------------------Generate GPS Object------------------------------------#
+    verbose = False
+    # 获取同震GPS数据
+    #gpsfile_6_4 = os.path.join('..', '..', '..', 'GPS', 'GPS_ENU6_4NoEW_CSI.dat')
+    #gpsfile_7_1 = os.path.join('..', '..', '..', 'GPS', 'GPS_ENU7_1NoEW_CSI.dat')
+
+    #cogps6_4 = gps(name='co6_4', utmzone=None, ellps='WGS84', lon0=lon0, lat0=lat0, verbose=verbose)
+    #cogps6_4.read_from_enu(gpsfile_6_4, factor=1., minerr=1., header=1, checkNaNs=True)
+    #cogps6_4.buildCd(direction='enu')
+
+    #cogps7_1 = gps(name='co7_1', utmzone=None, ellps='WGS84', lon0=lon0, lat0=lat0, verbose=verbose)
+    #cogps7_1.read_from_enu(gpsfile_7_1, factor=1., minerr=1., header=1, checkNaNs=True)
+    #cogps7_1.buildCd(direction='enu')
+
+    # gpsdata = [cogps6_4, cogps7_1]
+    # ----------------------------------Generate SAR Object-----------------------------------#
+
+    basedir = '..'
+    # 获取同震SAR数据
+    varres_t034d = os.path.join(basedir, r'InSAR', 'downsample', 'S1_T121D_ifg')
+    varres_t056a = os.path.join(basedir, r'InSAR', 'downsample', 'S1_T012A_ifg')
+
+    coDscsar = insar('S1_T121D_ifg', lon0=lon0, lat0=lat0, verbose=verbose)
+    coDscsar.read_from_varres(varres_t034d, cov=True)
+
+    coAscsar = insar('S1_T012A_ifg', lon0=lon0, lat0=lat0, verbose=verbose)
+    coAscsar.read_from_varres(varres_t056a, cov=True)
+
+    insardata = [coAscsar, coDscsar]
+    geodata = insardata 
+    #------------------------------Set ExploreFault Object-------------------------------------#
+    expfault = explorefault('invrc', lat0=lat0, lon0=lon0, config_file='default_config.yml', geodata=geodata, verbose=verbose)
+    nchains = expfault.nchains
+    chain_length = expfault.chain_length
+    expfault.setPriors(bounds=None, initialSample=None, datas=None) # datas for Sar reference
+    expfault.setLikelihood(datas=None, verticals=None) 
+    expfault.walk(nchains=nchains, chain_length=chain_length, comm=comm, filename='samples_mag_rake_multifaults.h5', 
+                  save_every=2, save_at_interval=False, covariance_epsilon = 1e-9, amh_a=1.0/9.0, amh_b=8.0/9.0)
+
+    # ---------------------------------Plot Results---------------------------------------------#
+    if rank == 0:
+        expfault.load_samples_from_h5(filename='samples_mag_rake_multifaults.h5')
+        expfault.print_mcmc_parameter_positions()
+        for ifault, faultname in enumerate(expfault.faultnames):
+            expfault.plot_kde_matrix(save=True, plot_faults=True, faults=faultname, fill=True, 
+                                    scatter=False, filename=f'kde_matrix_F{ifault}.png')
+        expfault.plot_kde_matrix(save=True, plot_faults=False, plot_sigmas=True, fill=True, 
+                                  scatter=False, filename='kde_matrix_sigmas.png')
+        faults = expfault.returnModels(model='mean')
+        # save the model results
+        expfault.save_model_to_file('model_results_mean.json', model='mean')
+        
+        # Plot GPS data
+        for fault in faults:
+            fault.color = 'b'
+        #for cogps in [cogps7_1]:
+            #cogps.buildsynth(faults, vertical=True)
+            #cogps.plot(faults=faults, drawCoastlines=True, data=['data', 'synth'], scale=0.2, legendscale=0.05, color=['k', 'r'],
+                    #seacolor='lightblue', box=[-119.5, -116, 34.3, 37], titleyoffset=1.02)
+            #cogps.fig.savefig(f'gps_{cogps.name}', ftype='png', dpi=600, 
+                              #bbox_inches='tight', mapaxis=None, saveFig=['map'])
+        #for cogps in [cogps6_4]:
+            #cogps.buildsynth(faults, vertical=True)
+            #cogps.plot(faults=faults, drawCoastlines=True, data=['data', 'synth'], scale=0.2, legendscale=0.05, color=['k', 'r'],
+                    #seacolor='lightblue', titleyoffset=1.02)
+            # cogps.fig.figCarte.savefig(f'gps_{cogps.name}_map.png', dpi=600, bbox_inches='tight')
+            #cogps.fig.savefig(f'gps_{cogps.name}', ftype='png', dpi=600, 
+                             #bbox_inches='tight', mapaxis=None, saveFig=['map'])
+        # Plot SAR data
+        for fault in faults:
+            fault.color = 'k'
+        for cosar in insardata:
+            cosar.buildsynth(faults, vertical=True)
+            for data in ['data', 'synth', 'res']:
+                cosar.plot(faults=faults, data=data, seacolor='lightblue', figsize=(3.5, 2.7),
+                           cbaxis=[0.15, 0.25, 0.25, 0.02], drawCoastlines=True, titleyoffset=1.02) # faults=faults
+                cosar.fig.savefig(f'sar_{cosar.name}_{data}', ftype='png', dpi=600, saveFig=['map'], 
+                                  bbox_inches='tight', mapaxis=None)
+                # cosar.fig.figCarte.savefig(f'sar_{cosar.name}_{data}_map.png', dpi=600, bbox_inches='tight')
+        print(expfault.model_dict)
+
